d50e3cda4f50cb0e831a3102df1298f6a85551c4
tentou <445138773@qq.com>
2016-09-05 14:03:45 +0800

    创建了一个验证服务器

---
+++ Nodejs/简单node服务器/app.js
@@ -0,0 +1,54 @@
+/**
+ * 创建一个验证微信Token的node服务器
+ * Created by zhangpeng on 2016/9/5.
+ */
+var http = require("http");
+var url = require("url");
+var crypto = require("crypto");
+
+function sha1(str){
+    var md5sum = crypto.createHash("sha1");
+    md5sum.update(str);
+    str = md5sum.digest("hex");
+    return str;
+}
+
+function validateToken(req,res){
+    var query = url.parse(req.url,true).query;
+    //console.log("*** URL:" + req.url);
+    //console.log(query);
+    var signature = query.signature;
+    var echostr = query.echostr;
+    var timestamp = query['timestamp'];
+    var nonce = query.nonce;
+    var oriArray = new Array();
+    oriArray[0] = nonce;
+    oriArray[1] = timestamp;
+    oriArray[2] = "tentouceshi";//这里是你在微信开发者中心页面里填的token，而不是****
+    oriArray.sort();
+    var original = oriArray.join('');
+    console.log("Original str : " + original);
+    console.log("Signature : " + signature );
+    var scyptoString = sha1(original);
+    if(signature == scyptoString){
+        res.end(echostr);
+        console.log("Confirm and send echo back");
+    }else {
+        res.end("false");
+        console.log("Failed!");
+    }
+}
+
+
+var webSvr = http.createServer(validateToken);
+webSvr.listen(3000,function(){
+    console.log("Start validate");
+});
+// var http = require('http');
+// var server = http.createServer();
+// server.on('request',function(req,res){
+//     res.writeHead(200,{'Content-Type':'text/plain'})
+//     res.end('hello world')
+// });
+// server.listen(3000);
+// console.log('服务器已启动成功')
